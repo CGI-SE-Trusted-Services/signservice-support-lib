a369775b3caf2da7369c45bf3e7d446c7b451690
Tobias Agerberg <tobias.agerberg@cgi.com>
2024-01-19 16:31:58 +0100

    EIDDSS-907 Updated headers

--- src/main/java/se/signatureservice/support/api/v2/SupportServiceLibraryException.java
+++ src/main/java/se/signatureservice/support/api/v2/SupportServiceLibraryException.java
@@ -3,9 +3,9 @@
  *  Signature Service - Support Service Library                          *
  *                                                                       *
  *  This software is free software; you can redistribute it and/or       *
- *  modify it under the terms of the GNU Affero General Public License   *
+ *  modify it under the terms of the GNU Lesser General Public License   *
  *  License as published by the Free Software Foundation; either         *
- *  version 3   of the License, or any later version.                    *
+ *  version 3 of the License, or any later version.                      *
  *                                                                       *
  *  See terms of license at gnu.org.                                     *
  *                                                                       *
--- src/main/java/se/signatureservice/support/common/cache/SimpleCacheProvider.java
+++ src/main/java/se/signatureservice/support/common/cache/SimpleCacheProvider.java
@@ -1,10 +1,9 @@
 /************************************************************************
  *                                                                       *
- *  Signservice Support Lib                                              *
+ *  Signature Service - Support Service Library                          *
  *                                                                       *
  *  This software is free software; you can redistribute it and/or       *
  *  modify it under the terms of the GNU Lesser General Public License   *
- *  (LGPL-3.0-or-later)                                                  *
  *  License as published by the Free Software Foundation; either         *
  *  version 3 of the License, or any later version.                      *
  *                                                                       *
