d1aa6d557834f460f1e0217b8c6f0492340f887a
filipwessman <filip.wessman@cgi.com>
2025-05-08 20:32:33 +0200

    license header fix for signservice-java-configuration, -support-lib and transactionsigning

--- src/main/java/se/signatureservice/support/template/TemplateProcessor.java
+++ src/main/java/se/signatureservice/support/template/TemplateProcessor.java
@@ -1,3 +1,15 @@
+/************************************************************************
+ *                                                                       *
+ *  Signature Service - Support Service Library                          *
+ *                                                                       *
+ *  This software is free software; you can redistribute it and/or       *
+ *  modify it under the terms of the GNU Lesser General Public License   *
+ *  License as published by the Free Software Foundation; either         *
+ *  version 3 of the License, or any later version.                      *
+ *                                                                       *
+ *  See terms of license at gnu.org.                                     *
+ *                                                                       *
+ *************************************************************************/
 package se.signatureservice.support.template;
 
 import java.util.Map;
